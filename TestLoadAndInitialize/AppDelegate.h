51c1a05d5b1099fc0252b8d4fd357ea53d15fedb
liyang <995720636@qq.com>
2016-04-08 10:21:28 +0800

    Initial Commit

---
+++ TestLoadAndInitialize/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestLoadAndInitialize
+//
+//  Created by liyang on 16/4/8.
+//  Copyright © 2016年 liyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
